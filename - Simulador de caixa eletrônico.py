443305bae7d4c0ccef2234089b7fcfd9226c1630
bieucordeiro <93162135+bieucordeiro@users.noreply.github.com>
2025-11-20 12:20:37 -0300

    Add files via upload

---
+++ - Simulador de caixa eletrônico.py
@@ -0,0 +1,26 @@
+print('=' *35)
+print('{:^30}'.format('BANCO CEV'))
+print('=' *35)
+v = int(input('Qual valor você quer sacar? R$ '))
+total = v
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print('Total de {} cédulas de R${}.'.format(totalced, ced))
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('=' *45)
+print('{:^30}'.format('Volte sempre ao banco CEV. Tenha um bom dia!'))
+print('=' *45)
